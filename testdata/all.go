29b05bd59e92323ebe090eb1801d516da8f0a17b
Daniel Martí <mvdan@mvdan.cc>
2017-03-04 23:17:17 +0100

    Add more tests

--- testdata/all.go
+++ testdata/all.go
@@ -1,9 +1,24 @@
 package foo
 
+import "net/http"
+
 func allUsed(a, b int) int {
 	return a + b
 }
 
 func oneUnused(a, b int) int {
 	return a
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
+}
+
+type fooIface interface {
+	foo(w http.ResponseWriter, code int) error
+}
+
+func fooImpl(w http.ResponseWriter, code int) error {
+	w.Write([]byte("hi"))
+	return nil
 }

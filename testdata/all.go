a6e1ff7fdc02aab739c9124e53edd06565111541
Daniel Martí <mvdan@mvdan.cc>
2017-03-04 23:29:10 +0100

    Add a test for an unused method receiver

--- testdata/all.go
+++ testdata/all.go
@@ -2,23 +2,29 @@ package foo
 
 import "net/http"
 
-func allUsed(a, b int) int {
+func AllUsed(a, b int) int {
 	return a + b
 }
 
-func oneUnused(a, b int) int {
+func OneUnused(a, b int) int {
 	return a
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hi"))
 }
 
-type fooIface interface {
+type FooIface interface {
 	foo(w http.ResponseWriter, code int) error
 }
 
-func fooImpl(w http.ResponseWriter, code int) error {
+func FooImpl(w http.ResponseWriter, code int) error {
 	w.Write([]byte("hi"))
 	return nil
+}
+
+type FooType int
+
+func (f FooType) AllUsed(a, b int) int {
+	return a + b
 }
